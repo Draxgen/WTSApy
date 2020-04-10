ddb9c5e3795fb1827a9b4dcbec27c14addfaf301
Draxgen <48406465+Draxgen@users.noreply.github.com>
2020-04-10 20:37:44 +0200

    Platform and first script
    Platform for running the scripts and first scripts added.
    Download Manager - manages the Download folder, by sorting downloaded fiels into category folders

---
+++ JTScriptsPlatform.py
@@ -0,0 +1,45 @@
+'''
+Creates a bat file with all python scripts and runs the bat file so the python scripts in parallel in the background. 
+This script should be added to windows' Task Scheduler to be run at Startup
+'''
+__author__ = 'Jan Tuziak'
+
+import os
+import sys
+import subprocess
+import glob
+
+if __name__ == '__main__':
+    #get this script name
+    this_script = os.path.basename(__file__)
+
+    # get working folder and set it
+    main_folder = os.path.join(sys.path[0], 'Scripts')
+    os.chdir(main_folder)
+
+    # get python scripts to run
+    scripts = []
+    for file in glob.glob('*.py'):
+        scripts.append(file)
+
+    # if no scripts found stop execution
+    if not scripts:
+        sys.exit()
+        
+    # create batch file text
+    bat = ''
+    for scr in scripts:
+        path = os.path.join(main_folder, scr)
+        bat += f'start "" pythonw "{path}"\n'
+
+    # create batch file
+    bat_path = os.path.join(main_folder, 'scripts.bat')
+    f = open(bat_path, 'w')
+    f.write(bat)
+    f.close()
+
+    # call batch file
+    subprocess.call(bat_path)
+
+    # remove batdh file
+    os.remove(bat_path)    
---
+++ Scripts/downloadmanager.py
@@ -0,0 +1,85 @@
+import time
+import os
+import logging
+import sys
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+import os.path
+
+class FileOrganizerEventHandler(FileSystemEventHandler):
+    
+    # folders and their extensions
+    _folderAndExt = {
+        "Coding" : ['.vi','.py','.c','.cpp','.java'],
+        "Images" : ['.jpg','.jpeg','.bmp','.gif','.png'],
+        "Documents" : ['.doc','.docx','.txt','.ppt','.xlsx'],
+        "Installers" : ['.exe','.msi'],
+        "Audio" : ['.mp3','.flac','.wav'],
+        "Video" : ['.mp4'],
+        "Torrents" : ['.torrent']
+    }
+
+    def on_created(self, event):
+        # ignore new folders
+        if event.is_directory:
+            return
+        
+        # recognize the file
+        breakLoop = False
+        file_name = os.path.basename(event.src_path)
+        folder_path = os.path.dirname(event.src_path)
+        file_name_no_ext = os.path.splitext(file_name)[0]
+        file_ext = os.path.splitext(file_name)[1]
+        for key in self._folderAndExt:
+            for x in self._folderAndExt[key]:
+                if x == file_ext:
+                    # create a new folder if it doesn't exist
+                    if not os.path.exists(os.path.join(folder_path, key)):
+                        os.makedirs(os.path.join(folder_path, key))
+                    # move the file to correct folder
+                    timeout = time.time() + 10 # wait 10s max for access to file 
+                    target_path = os.path.join(folder_path, key, file_name)
+                    while True:
+                        try:
+                            # if file with that name exists add ' - x' at the end
+                            for x in range(10):
+                                if os.path.exists(target_path):
+                                    target_path = os.path.join(folder_path, key, (file_name_no_ext + '(' + str(x+2) + ')' + file_ext))
+                                    continue
+                                os.rename(event.src_path, target_path)
+                                break
+                            break
+                        except Exception as ex:
+                            print(ex)
+                            time.sleep(1)
+                        if time.time() > timeout:
+                            break
+                    # set breakLoop variable to break the outer for loop
+                    breakLoop = True
+                    break
+            if breakLoop: break
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, 
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    
+    # get Downloads folder path
+    downloadFolder = os.path.expanduser(r'~\Downloads')
+
+    # listen for new file events
+    event_handler = FileOrganizerEventHandler()
+    
+    # start Observer
+    observer = Observer()
+    observer.schedule(event_handler, downloadFolder)
+    observer.start()
+    
+
+    # start endless loop (interuppted by ctrl-C)
+    try:
+        while True:
+            time.sleep(1)
+    except(KeyboardInterrupt):
+        observer.stop()
+    observer.join()
